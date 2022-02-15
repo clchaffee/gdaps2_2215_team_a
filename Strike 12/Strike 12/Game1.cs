d543d8a89600e2885568644089a32ac912393618
conra <conra@DESKTOP-400R05M>
2022-02-14 19:21:18 -0500

    Hard coded, could use a method for the key input but since it's just for demonstration, this works

---
+++ Strike 12/Strike 12/Game1.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Strike_12
+{
+    enum GameState
+    {
+        Menu,
+        Controls,
+        Arena,
+        Shop
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont titleFont;
+        private SpriteFont displayFont;
+        GameState state = GameState.Menu;
+        KeyboardState kbState;
+        KeyboardState prevKbState = Keyboard.GetState();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            titleFont = Content.Load<SpriteFont>("Title");
+            displayFont = Content.Load<SpriteFont>("Display");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //gets keyboard state for each frame
+            kbState = Keyboard.GetState();
+
+            
+            //switch for the states
+            switch (state)
+            {
+                //if enter is pressed in menu, starts the game; if space is pressed opens the control screen
+                case GameState.Menu:
+                    if (kbState.IsKeyDown(Keys.Enter) && prevKbState.IsKeyUp(Keys.Enter))
+                    {
+                        state = GameState.Arena;
+                    }
+                    if (kbState.IsKeyDown(Keys.Space) && prevKbState.IsKeyUp(Keys.Space))
+                    {
+                        state = GameState.Controls;
+                    }
+                    break;
+                //while in the control screen, press enter to return to the menu
+                case GameState.Controls:
+                    if (kbState.IsKeyDown(Keys.Space) && prevKbState.IsKeyUp(Keys.Space))
+                    {
+                        state = GameState.Menu;
+                    }
+                    break;
+                // when in the arena, "dies" when you press space, entering the shop
+                case GameState.Arena:
+                    if (kbState.IsKeyDown(Keys.Space) && prevKbState.IsKeyUp(Keys.Space))
+                    {
+                        state = GameState.Shop;
+                    }
+                    break;
+                //if enter is pressed in the shop, returns to arena; if space is pressed brings up the menu
+                case GameState.Shop:
+                    if (kbState.IsKeyDown(Keys.Enter) && prevKbState.IsKeyUp(Keys.Enter))
+                    {
+                        state = GameState.Arena;
+                    }
+                    if (kbState.IsKeyDown(Keys.Space) && prevKbState.IsKeyUp(Keys.Space))
+                    {
+                        state = GameState.Menu;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            prevKbState = kbState;
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //sets background to gold and opens the sprite batch
+            GraphicsDevice.Clear(Color.Goldenrod);
+            _spriteBatch.Begin();
+
+            //depending on the states, displays the appropriate text 
+            switch (state)
+            {
+                case GameState.Menu:
+                    _spriteBatch.DrawString(titleFont, "STRIKE XII",
+                        new Vector2(200, 200), Color.Black);
+                    _spriteBatch.DrawString(displayFont, "Press Enter to continue\nTo learn the controls, press Space",
+                        new Vector2(100, 400), Color.Black);
+                    break;
+                case GameState.Controls:
+                    _spriteBatch.DrawString(titleFont, "Filler for Controls page",
+                        new Vector2(150, 200), Color.Black);
+                    _spriteBatch.DrawString(displayFont, "Press Space to continue to return to the menu",
+                        new Vector2(100, 400), Color.Black);
+                    break;
+                case GameState.Arena:
+                    _spriteBatch.DrawString(titleFont, "Filler for Arena",
+                        new Vector2(150, 200), Color.Black);
+                    _spriteBatch.DrawString(displayFont, "Press Space to go to the shop page (happens upon character death)",
+                        new Vector2(100, 400), Color.Black);
+                    break;
+                case GameState.Shop:
+                    _spriteBatch.DrawString(titleFont, "Filler for Shop page",
+                        new Vector2(150, 200), Color.Black);
+                    _spriteBatch.DrawString(displayFont, "Press Enter to return to the arena\nPress Space to return to the menu",
+                        new Vector2(100, 400), Color.Black);
+                    break;
+                default:
+                    break;
+            }
+
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
