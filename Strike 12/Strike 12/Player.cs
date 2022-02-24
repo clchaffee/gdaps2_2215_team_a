708a311fe8034f58c53e55b21e817053eda68942
conra <conra@DESKTOP-400R05M>
2022-02-23 19:26:35 -0500

    Gave player class some basics, update and a paramaterized ctor

--- Strike 12/Strike 12/Player.cs
+++ Strike 12/Strike 12/Player.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 /// <summary>
 /// Author: Copious Cats 
 /// Purpose: This is a class that will inherit from the GameObject 
@@ -8,11 +11,20 @@ using System.Text;
 /// </summary>
 namespace Strike_12
 {
-    class Player // : GameObject
+    class Player : GameObject
     {
         // ----- | Fields | -----
+        private KeyboardState kbState;
+        //parameters for size and position for the player not currently decided
+
 
         // ----- | Constructor | -----
+        public Player(Texture2D texture, Rectangle position)
+            :base(texture, position)
+        {
+
+        }
+
 
         // Paramatarized Constructor
 
@@ -22,8 +34,22 @@ namespace Strike_12
 
         // -- Methods Overriden from parent class
 
-        // Abstract Update(GameTime gameTime):
+        /// <summary>
+        /// Update contains logic for moving the character left and right for now
+        /// needs to be updated, probably by Colby, in order to simulate jumping
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            kbState = Keyboard.GetState();
 
-        // Draw(spriteBatch sb):
+            if (kbState.IsKeyDown(Keys.D))
+            { position.X += 1; }
+            if (kbState.IsKeyDown(Keys.A))
+            { position.X -= 1; }
+
+        }
+
+
     }
 }
