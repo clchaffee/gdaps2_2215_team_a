663845e8707922c2e880fb710c329839bf9a94f2
amd4149 <amd4149@rit.edu>
2022-04-22 14:20:24 -0400

    deleted stuff

--- Strike 12/Strike 12/Game1.cs
+++ Strike 12/Strike 12/Game1.cs
@@ -1653,14 +1653,6 @@ namespace Strike_12
                                     player.timeStopPurchased = true;
                                     shop.TimeSlow = true;
                                     break;
-
-                                case "Heal":
-                                    button.Cost += 10;
-                                    break;
-
-                                case "Slow":
-                                    button.Cost += 10;
-                                    break;
                             }
                         }
                     }
